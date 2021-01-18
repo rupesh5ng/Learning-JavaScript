8fd27d9af3281be20fe5df797376a50048f66044
rupesh5ng <rupesh_singh87@yahoo.com>
2021-01-18 12:17:07 -0600

    finished all function_exercise

---
+++ function/C_function_exercise/contains.js
@@ -0,0 +1,11 @@
+// Write a function `contains(str1, str2)` that accepts two strings as arguments. The function should
+// return a boolean indicating whether or not `str2` is contained within `str1`. The function should
+// ignore any differences in capitalization.
+let contains = function(str1, str2) {
+  return (str1.toLowerCase()).indexOf(str2.toLowerCase()) > -1;
+}
+console.log(contains("caterpillar", "pill")); // true
+console.log(contains("lion's share", "on")); // true
+console.log(contains("SORRY", "or")); // true
+console.log(contains("tangent", "gem")); // false
+console.log(contains("clock", "ok")); // false
