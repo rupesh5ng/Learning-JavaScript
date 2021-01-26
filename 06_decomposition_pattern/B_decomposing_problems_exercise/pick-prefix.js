4ba18973beaa61002e4d23f37fdf38fd04474c45
rupesh5ng <rupesh_singh87@yahoo.com>
2021-01-25 23:17:50 -0600

    finished part B of Decomposition exercise

--- 06_decomposition_pattern/B_decomposing_problems_exercise/pick-prefix.js
+++ 06_decomposition_pattern/B_decomposing_problems_exercise/pick-prefix.js
@@ -1,6 +1,14 @@
 // Write a function `pickPrefix(strings, prefix)` that accepts an array of strings and a prefix string 
 // as an argument. The function should return the an array of words that begin with the prefix.
-
+let pickPrefix = function(strings, prefix){
+  let newStrings =[];
+  strings.forEach(function(word) {
+    if (word.indexOf(prefix) === 0) {
+      newStrings.push(word);
+    }
+  });
+  return newStrings;
+}
 console.log(pickPrefix(['connect', 'company', 'concert', 'cram'], 'con'));
 // ['connect', 'concert']
 
--- 06_decomposition_pattern/B_decomposing_problems_exercise/trendy-text.js
+++ 06_decomposition_pattern/B_decomposing_problems_exercise/trendy-text.js
@@ -1,6 +1,26 @@
 // Write a function `trendyText` that accepts a sentence string as an argument. The function should
 // return the sentence where the last vowel of every word is removed.
 
-console.log(trendyText("the concert will be epic")); // 'th concrt wll be epc'
+
+let removeLastVowel = function(string) {
+  let vowels ="aeiou";
+  for (let i = string.length - 1; i >= 0; i-- ){
+    if (vowels.includes(string[i]))
+    {return string.slice(0,i) + string.slice(i+1)}
+  }
+  return string;
+};
+
+
+let trendyText = function(sent ){
+  let words = sent.split(" ")
+  let newWords = [];
+  words.forEach(function(word){
+    newWords.push(removeLastVowel(word));
+  })
+  return newWords.join(" ")
+};
+
+console.log(trendyText("the concert will be epic")); // 'th concrt wll b epc'
 console.log(trendyText("breakfast food is wonderful")); // 'breakfst fod s wonderfl'
 console.log(trendyText("the weather will improve hopefully")); // 'th weathr wll improv hopeflly'
