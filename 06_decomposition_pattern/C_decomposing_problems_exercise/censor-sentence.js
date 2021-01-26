9a2eb85822751585f262491ec66e7c8015429e86
rupesh5ng <rupesh_singh87@yahoo.com>
2021-01-25 23:50:26 -0600

    finished Decomposition Exercise part C

--- 06_decomposition_pattern/C_decomposing_problems_exercise/censor-sentence.js
+++ 06_decomposition_pattern/C_decomposing_problems_exercise/censor-sentence.js
@@ -2,7 +2,20 @@
 // of target words as arguments. The function should return a new sentence where any target words 
 // have all of their characters replaced with '*'s.
 
+let censorSentence = function(sentence, targetWords) {
+  let words = sentence.split(" ");
+  let newWords = [];
+  words.forEach(function(word) {
+    if ( targetWords.includes(word)) {
+      newWords.push("*".repeat(word.length));
+    }else{
+      newWords.push(word);
+    }
+  })
+  return newWords.join(" ");
+};
+
 console.log(censorSentence('where the heck is my celery', ['heck', 'celery'])); 
 // 'where the **** is my ******'
 
-console.log(censorSentence('why you little sweetheart', ['sweetheart', 'salad'])); 
+console.log(censorSentence('why you little sweetheart', ['sweetheart', 'salad'])); // 'why you little **********'
